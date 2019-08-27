f5285f43d9a979375aa775fcee4acb330d485fc5
Tom Steinmeier <neuro.genesis67@gmail.com>
2019-08-27 14:55:47 +0200

    dunnnolol

---
+++ DeliciousDelegations/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public delegate int StringToInt(string s);
+
+namespace DeliciousDelegations {
+    // _______________  Exercise 5  _________________________
+    // Create a new class EventClass. 
+    // Give this class a StringToInt delegate that takes a string argument and returns an integer. 
+    // Implement a method CountChars, that count chars in a string argument and returns the integer result.
+
+    // _______________  Exercise 6  _________________________
+    // Create a StringToInt delegate from your CountChars method. 
+    // Use the delegate to count the chars in 3 different strings. 
+
+    // _______________  Exercise 7  _________________________
+    // Create a method DelegateUser that takes two arguments: 
+    // a StringToInt delegate and a string argument. 
+    // DelegateUser must call the delegate with the string argument and return the result.
+    // Demonstrate that you can use your DelegateUser on a string.
+
+    class EventClass {
+        public static void Main () {
+            StringToInt sti = new StringToInt(CountChars);
+            Console.WriteLine(sti.Invoke("I ate a bear. Who wins now huh?"));
+            Console.WriteLine(sti.Invoke("I used to be a strong man, now I'm stronger!"));
+            Console.WriteLine(sti.Invoke("I found money in my asscrack the other day. I suspect fame will reach me any moment now"));
+            Console.WriteLine(DelegateUser(sti, "Yes baby, turn me into a pamphlet"));
+            Console.ReadKey();
+        }
+        public static int CountChars(string s) {
+            return s.Length;
+        }
+        public static int DelegateUser(StringToInt sti, string str) {
+            int result = sti(str);
+            return result;
+        }
+    }
+}
--- FindAll/Program.cs
+++ FindAll/Program.cs
@@ -38,9 +38,9 @@ namespace FindAll {
                 "Age: {1, -10}" +
                 "Weight: {2, -10}" +
                 "Score: {3, -10}",
-                this.Name, 
-                this.Age, 
-                this.Weight, 
+                this.Name,
+                this.Age,
+                this.Weight,
                 this.Score);
         }
 
@@ -77,45 +77,49 @@ namespace FindAll {
                 List<Person> data2 = Person.ReadCSVFile(dataLocation2);
 
                 // _______________  Exercise 1  _________________________
+                // a.All persons with a score below 2
+                // b.All persons with even score
+                // c.All persons with even score and weight above 60
+                // d.All persons with a weight divisible by 3
+
                 List<Person> e1a = data1.FindAll(p => p.Score > 2);
                 List<Person> e1b = data1.FindAll(p => p.Score % 2 != 1);
                 List<Person> e1c = data1.FindAll(p => p.Score % 2 != 1 && p.Weight > 60);
                 List<Person> e1d = data1.FindAll(p => p.Weight % 3 == 0);
 
                 // Person.PrintList(e1d);
-                   
-                // a.All persons with a score below 2
-                // b.All persons with even score
-                // c.All persons with even score and weight above 60
-                // d.All persons with a weight divisible by 3
 
 
                 // _______________  Exercise 2  _________________________
-                int index_a = data1.FindIndex(p => p.Score == 3);
-                int index_b = data1.FindIndex(p => p.Age < 10 && p.Score == 3);
-                persons     = data1.FindAll(  p => p.Age < 10 && p.Score == 3);
-                int index_d = data1.FindIndex(p => p.Age < 8  && p.Score == 3); // No such thing
-                // e: (The return value)
-                // System.ArgumentOutOfRangeException: 
-                // 'Index was out of range. 
-                // Must be non-negative and less than the size of the collection.
-                // Parameter name: index'
-
-                //Console.WriteLine(data1[index_d]);
-                // Console.WriteLine(persons.Count);
-                // Person.PrintList(data1);
-
                 // a.Use the FindIndex method on the List<T> class to find the index of the first person with a Score of 3.
                 // b.Use FindIndex to find the index of the first person under 10 years, with a score of 3.
                 // c.How many people are there under 10 years, with a score of 3?
                 // d.Use FindIndex to find the index of the first person under 8 years, with a score of 3.
                 // e.What does the return value mean?
 
+                int index_a = data1.FindIndex(p => p.Score == 3);
+                int index_b = data1.FindIndex(p => p.Age < 10 && p.Score == 3);
+                persons = data1.FindAll(p => p.Age < 10 && p.Score == 3);
+                int index_d = data1.FindIndex(p => p.Age < 8 && p.Score == 3); // No such thing
+                                                                               // e: (The return value)
+                                                                               // System.ArgumentOutOfRangeException: 
+                                                                               // 'Index was out of range. 
+                                                                               // Must be non-negative and less than the size of the collection.
+                                                                               // Parameter name: index'
+
+                //Console.WriteLine(data1[index_d]);
+                // Console.WriteLine(persons.Count);
+                // Person.PrintList(data1);
+
 
                 // _______________  Exercise 3  _________________________
-                List<Person> E3 = data1.SetAccepted(p => p.Score >= 6 && p.Age <= 40);
-                Person.PrintList(E3);
+                //Write an extension method on the List <Person> class so you can make this call:
+                //people.SetAccepted(p => p.Score >= 6 && p.Age <= 40);
+                //(Hint: This is not a simple problem)
 
+                //List<Person> E3 = data1.SetAccepted(p => p.Score >= 6 && p.Age <= 40);
+                //Person.PrintList(E3);
+                //Console.ReadKey();
                 //public static class ExtensionMethods {
                 //    public static List<Person> SetAccepted(this List<Person> persons, Func<Person, bool> logicAppliesTo) {
                 //        List<Person> newList = new List<Person>();
@@ -127,19 +131,99 @@ namespace FindAll {
                 //    }
                 //}
 
-
                 // _______________  Exercise 4  _________________________
+                //Sorting with List<T>.Sort()
+                //Sort persons after both Score and Age, (both ascending and descending)
+
+                //var sortList = data1;
+                ////sortList.Sort(new Sort_ScoreAge_Ascending());
+                //sortList.Sort(new Sort_ScoreAge_Descending());
+                //Person.PrintList(sortList);
+                //Console.ReadKey();
+
+                //public class Sort_ScoreAge_Ascending : IComparer<Person> {
+                //    public int Compare(Person a, Person b) {
+                //        if (a.Score > b.Score) {
+                //            return 1;
+                //        } else if (a.Score < b.Score) {
+                //            return -1;
+                //        }
+
+                //        if (a.Age > b.Age) {
+                //            return 1;
+                //        } else if (a.Age < b.Age) {
+                //            return -1;
+                //        } else {
+                //            return 0;
+                //        }
+                //    }
+                //}
+
+                //public class Sort_ScoreAge_Descending : IComparer<Person> {
+                //    public int Compare(Person a, Person b) {
+                //        if (a.Score < b.Score) {
+                //            return 1;
+                //        } else if (a.Score > b.Score) {
+                //            return -1;
+                //        }
+
+                //        if (a.Age < b.Age) {
+                //            return 1;
+                //        } else if (a.Age > b.Age) {
+                //            return -1;
+                //        } else {
+                //            return 0;
+                //        }
+                //    }
+                //}
 
 
+                // _______________  Exercise 5  _________________________
+                //Create a new class EventClass. 
+                //Give this class a StringToInt delegate that takes a string argument and returns an integer. 
+                //Implement a method CountChars, that count chars in a string argument and returns the integer result.
 
-
-
-
-
-
-
+                // See folder E05
+                
                 Console.ReadKey();
             }
         }
     }
+
+    
+    public class Sort_ScoreAge_Ascending : IComparer<Person> {
+        public int Compare(Person a, Person b) {
+            if (a.Score > b.Score) {
+                return 1;
+            } else if (a.Score < b.Score) {
+                return -1;
+            }
+
+            if (a.Age > b.Age) {
+                return 1;
+            } else if (a.Age < b.Age) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+    public class Sort_ScoreAge_Descending : IComparer<Person> {
+        public int Compare(Person a, Person b) {
+            if (a.Score < b.Score) {
+                return 1;
+            } else if (a.Score > b.Score) {
+                return -1;
+            }
+
+            if (a.Age < b.Age) {
+                return 1;
+            } else if (a.Age > b.Age) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
 }
+
